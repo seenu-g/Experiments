ea879c639f54995352c0534e69b738423cb9d242
gsvasan-g <srinivasan.g@accionlabs.com>
2018-11-13 19:55:31 +0530

    added function of func

--- go_examples/types/functions.go
+++ go_examples/types/functions.go
@@ -18,6 +18,16 @@ func FibonacciRecursion(n int) int {
 	return FibonacciRecursion(n-1) + FibonacciRecursion(n-2)
 }
 
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+func Fibonacci(n int) int {
+	return FibonacciRecursion(n)
+}
 func main() {
 	hypot := func(x, y float64) float64 {
 		return math.Sqrt(x*x + y*y)
@@ -28,8 +38,19 @@ func main() {
 	fmt.Println(math.Pow)
 	fmt.Println(compute(math.Pow))
 
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
 	for i := 0; i <= 19; i++ {
 		fmt.Printf("%d  ", FibonacciRecursion(i))
+	}
+	fmt.Println("")
+	for i := 0; i <= 19; i++ {
+		fmt.Printf("%d  ", Fibonacci(i))
 	}
 	fmt.Println("")
 
