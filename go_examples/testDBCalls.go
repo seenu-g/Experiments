c24a8f3687e6300d681048eda61ba5569195ae84
gsvasan-g <srinivasan.g@accionlabs.com>
2018-11-13 14:34:50 +0530

    connect to postgres

---
+++ go_examples/testDBCalls.go
@@ -0,0 +1,36 @@
+//https://www.calhoun.io/updating-and-deleting-postgresql-records-using-gos-sql-package/
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "Accion@123"
+	dbname   = "postgres"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+}
