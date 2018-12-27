78a580e653425bf433faf38ddd2fc34e5c49da0a
gsvasan-g <srinivasan.g@accionlabs.com>
2018-12-27 19:40:28 +0530

    add files

---
+++ graphql_examples/server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+
+const app = express();
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema, // pass in a schema property
+    graphiql: true
+
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});  //http://localhost:4000/graphql
