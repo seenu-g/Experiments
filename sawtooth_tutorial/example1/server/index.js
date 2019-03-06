12376f8be55fa3fba72e0b38c522d47c10e1be37
seenu-g <srinivasan.gsvasan@gmail.com>
2019-03-06 16:58:13 +0530

    add files

---
+++ sawtooth_tutorial/example1/server/index.js
@@ -0,0 +1,12 @@
+const { TransactionProcessor } = require('sawtooth-sdk/processor')
+
+const IntegerKeyHandler = require('./intkey_handler')
+const env = require('./env')
+
+const transactionProcessor = new TransactionProcessor(env.validatorUrl)
+
+transactionProcessor.addHandler(new IntegerKeyHandler())
+transactionProcessor.start()
+
+console.log(`Starting intkey transaction processor`)
+console.log(`Connecting to Sawtooth validator at ${env.validatorUrl}`)
