8adf6bb7eacfac0672617ea5ddf8a8c3f7271401
seenu-g <srinivasan.gsvasan@gmail.com>
2019-04-25 15:49:43 +0530

    add files

---
+++ go_examples/channel/2channels.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+func square(c chan int) {
+	fmt.Println("[square] reading")
+	num := <-c
+	c <- num * num
+}
+
+func cube(c chan int) {
+	fmt.Println("[cube] reading")
+	num := <-c
+	c <- num * num * num
+}
+
+func main() {
+	fmt.Println("[main] main() started")
+
+	squareChan := make(chan int)
+	cubeChan := make(chan int)
+
+	go square(squareChan)
+	go cube(cubeChan)
+
+	testNum := 3
+	fmt.Println("[main] sent testNum to squareChan")
+	squareChan <- testNum
+	fmt.Println("[main] resuming")
+
+	fmt.Println("[main] sent testNum to cubeChan")
+	cubeChan <- testNum
+	fmt.Println("[main] resuming")
+
+	fmt.Println("[main] reading from channels")
+	squareVal, cubeVal := <-squareChan, <-cubeChan
+	sum := squareVal + cubeVal
+	fmt.Println("[main] sum of square and cube of", testNum, " is", sum)
+	fmt.Println("[main] main() stopped")
+
+	go square(squareChan)
+	go cube(cubeChan)
+	testNum = 4
+	fmt.Println("[main] sent testNum to squareChan")
+	squareChan <- testNum
+	fmt.Println("[main] resuming")
+
+	fmt.Println("[main] sent testNum to cubeChan")
+	cubeChan <- testNum
+	fmt.Println("[main] resuming")
+
+	fmt.Println("[main] reading from channels")
+	squareVal, cubeVal = <-squareChan, <-cubeChan
+	sum = squareVal + cubeVal
+	fmt.Println("[main] sum of square and cube of", testNum, " is", sum)
+	fmt.Println("[main] main() stopped")
+}
