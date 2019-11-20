4bbe27de2e578f10214d96b1ab86a92e3f2a972d
G <gsvasan@yahoo.com>
2019-11-20 23:05:47 +0530

    Update list.py

--- py_examples/list.py
+++ py_examples/list.py
@@ -9,6 +9,23 @@ def printList(list):
     for item in list:
         print(item)
         
+def print_employee(employee_list):
+    print("Employee No,  Employee Name")
+    total_salary = 0
+    for employee in employee_list:
+        print(employee[1],employee[0],employee[2])
+        total_salary = total_salary + int(employee[2])
+    print("Total salary of employees :", total_salary)
+
+def print_employee2(employee_list):
+    print("Employee No  Employee Name Salary")
+    total_salary = 0
+    for index in range(0,len(employee_list)):
+        print(employee_list[index][1],employee_list[index][0],employee_list[index][2])
+        total_salary = total_salary + int(employee_list[index][2])
+
+    print("Total salary of employees :", total_salary)
+    
 # List is a collection which is ordered and changeable. Allows duplicate members.
 thislist = ["apple", "banana", "cherry"]
 thislist.append("orange")
@@ -66,3 +83,7 @@ try:
     print(thislist)
 except:
     print("Exception shown as the program tried to print empty list")
+    
+employees =[["Suraj",1,20000],["Naveen",2,20000],["Neha",3,20000],["Meghna",4,20000]]
+#print_employee(employees)
+print_employee2(employees)
