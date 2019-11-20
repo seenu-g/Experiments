7121f1ce792aa69e1c4423f0382508d864c894be
G <gsvasan@yahoo.com>
2019-11-20 22:26:11 +0530

    Update list.py

--- py_examples/list.py
+++ py_examples/list.py
@@ -1,3 +1,14 @@
+def average(list):
+   total = 0
+   for x in range(len(list)):
+       total = total + x
+   average = total/len(list)
+   print("Average of list", list, "with", len(list),"elements is", average)
+  
+def printList(list):
+    for item in list:
+        print(item)
+        
 # List is a collection which is ordered and changeable. Allows duplicate members.
 thislist = ["apple", "banana", "cherry"]
 thislist.append("orange")
@@ -21,6 +32,9 @@ print("sort list in reverse: ", thislist)
 if "apple" in thislist:
       print("Yes, 'apple' is in the fruits list")
 
+if "brinjal" not in thislist: 
+      print("Yes, 'brinjal' is not in the fruits list")
+
 print("Count : ",len(thislist))
 
 popfruit = thislist.pop()
@@ -34,13 +48,17 @@ print(thislist)
 
 
 thatlist = range(5,125,10)
-print(thatlist)
-thatlist.reverse()
-print("reversed list: ",thatlist)
-thatlist.sort()
-print("sorted list: ",thatlist)
-thatlist.sort(reverse=True)
-print("sort list in reverse: ", thatlist)
+for x in range(len(thatlist)):
+    print (thatlist[x])
+print(*thatlist)
+average(thatlist)
+printList(thatlist)
+#thatlist.reverse()
+#print("reversed list: ",thatlist)
+#thatlist.sort()
+#print("sorted list: ",thatlist)
+#thatlist.sort(reverse=True)
+#print("sort list in reverse: ", thatlist)
 
 del thislist[0]
 try:
