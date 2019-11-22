b89234c1ecd22e4c95a4b93daf4af3cce4afb418
G <gsvasan@yahoo.com>
2019-11-22 16:49:39 +0530

    Update random.py

--- py_examples/random.py
+++ py_examples/random.py
@@ -32,7 +32,42 @@ def poem():
         print(the_sentence)
         index = index + 1
 
+def make_random():
+    numbers =[]
+    for i in range(0,10):
+        numbers.append(random.randint(1,100)) #use random.random() to real numbers
+    return numbers
+
+def make_same_random():
+    numbers =[]
+    random.seed(17)
+    for i in range(0,10):
+        numbers.append(random.randint(1,100)) #use random.random() to real numbers
+    return numbers
+    
 sentence()
 print(random.random())
 print(random.randint(3,30))
 poem()    
+
+random_integers = make_random()
+print("the list of random integers", random_integers)
+random_integers = make_random()
+print("the list of random integers", random_integers)
+
+
+random_same = make_same_random()
+print("the list of random same", random_same)
+random_same = make_same_random()
+print("the list of random same", random_same)
+
+list_for_sort = random_integers
+print("the list for sorting", list_for_sort)
+sorted_list = list_for_sort.sort()
+print("the sorted list", list_for_sort)
+
+alpha_list = ['x','a','w','d','p']
+print("the provided alpha list", alpha_list)
+alpha_list.sort() #alpha_list.sort(key=str.lower)
+print("the sorted alpha list", alpha_list)
+
