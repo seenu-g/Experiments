6924e26b8bc6d9950226ba39fc1f950caafdca80
seenu-g <srinivasan.gsvasan@gmail.com>
2020-04-20 18:23:03 +0530

    add file

--- py_examples/torch/basic_torch.py
+++ py_examples/torch/basic_torch.py
@@ -12,9 +12,49 @@ def basic_func() :
     print("second column", x[:, 1]) # display second column
     print("second row", x[1, :]) # display second row
 
-    print("cuda is_available: ",torch.cuda.is_available())
     x = torch.zeros(5, 3, dtype=torch.long)
     print(x)
+
+def gradient_func() :
+    a = torch.ones((2,2), requires_grad=True)
+    print(a)
+    
+    b = a + 5  #added 5 to all the elements of this tensor 
+    c = b.mean() #  mean of that tensor
+    print(b)
+    print(c)
+    
+    # back propagating
+    c.backward() 
+    # # computing gradients
+    print("Gradient of a",a.grad)
+
+def basic_operations():
+    a = torch.zeros((3,3))
+    print(a)
+    print(a.shape)
+    # random.randn() function returns random numbers that follow a standard normal distribution. 
+    a = torch.randn(3,3)
+    print(a)
+    print("type:",type(a))
+    # transpose
+    b = torch.t(a)
+    print("Transpose \n",b)
+
+    print("Add \n", torch.add(a,b))
+    print("Sub \n", torch.sub(a,b))
+    print("Mul \n", torch.mul(a,b))
+    print("Div \n", torch.div(a,b))
+
+def concat() :
+    a = torch.tensor([[1,2],[3,4]])
+    b = torch.tensor([[5,6],[7,8]])
+    # concatenating vertically // add as rows
+    row_merge = torch.cat((a,b))
+    print(row_merge, row_merge.shape)
+    # concatenating horizontally // add as columns
+    col_merge = torch.cat((a,b),dim=1)
+    print(col_merge,col_merge.shape)
 
 def add() :
     x = torch.tensor([5.5, 3]) #initailize tensor using data
@@ -51,11 +91,14 @@ def numpy_to_tensor() :
 def main() :
     use_cuda = torch.cuda.is_available()
     print("use_cuda = torch.cuda.is_available() :", use_cuda)
+    gradient_func()
     #basic_func()
+    #basic_operations()
+    #concat()
     #add()
     #load_boston_data()
-    tensor_to_numpy()
-    numpy_to_tensor()
+    #tensor_to_numpy()
+    #numpy_to_tensor()
     
 # this means that if this script is executed, then 
 # main() will be executed
