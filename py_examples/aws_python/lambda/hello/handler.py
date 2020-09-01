622f770457ed54353fee7eac7ecb9c4f82bb11ef
seenu-g <srinivasan.gsvasan@gmail.com>
2020-09-01 19:45:21 +0530

    Add files

--- py_examples/aws_python/lambda/hello/handler.py
+++ py_examples/aws_python/lambda/hello/handler.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import time
 
 
 def hello(event, context):
@@ -12,5 +13,17 @@ def hello(event, context):
         "statusCode": 200,
         "body": json.dumps(body)
     }
+    
+    print("Function Name, Function Version :",context.function_name, context.function_version)
+    print("invoked_function_arn: ",context.invoked_function_arn)
 
+    print("Log stream name: ", context.log_stream_name)
+    print("Log group name: ",  context.log_group_name)
+    print("Request ID: ",context.aws_request_id)
+        
+    print("Mem. limits(MB):", context.memory_limit_in_mb)
+    #  Code will execute quickly, so we add a 1 second intentional delay so you can see that in time remaining value.
+    time.sleep(5) 
+    print("Time remaining (MS):", context.get_remaining_time_in_millis())
+    
     return response
