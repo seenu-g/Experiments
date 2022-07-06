5c0cea1898b1ef9f01354aa43c4a1e519542122a
seenu-g <srinivasan.gsvasan@gmail.com>
2022-07-06 18:58:46 +0530

    Add payroll files

---
+++ java_examples/payroll/src/main/java/com/payroll/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ java_examples/payroll/src/test/java/com/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
