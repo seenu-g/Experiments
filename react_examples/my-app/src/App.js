7dfe392ef07e32889521c7f275ddeac7bc24317b
seenu-g <44487836+seenu-g@users.noreply.github.com>
2022-10-10 18:26:45 +0530

    Create App.js

---
+++ react_examples/my-app/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Garage from './App.js';
+import MyForm from './myForm.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render (<MyForm />)
+ root.render(<Garage />);
